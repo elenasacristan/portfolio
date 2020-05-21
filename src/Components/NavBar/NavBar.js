2343a10bb987b8808ebb194f4515bbda634e460e
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-21 15:54:25 +0100

    Add logo and improve styles

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,32 +1,33 @@
 import React from "react";
 import "./NavBar.css";
+import { FaCode } from "react-icons/fa";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <nav className="NavBar">
       <div className="logo">
-        <span>REPLACE with LOGO</span>
+        <span>
+          <Link to="/"><FaCode className="logo-icon" color="#ffc355" size="3.2rem" /></Link>
+        </span>
       </div>
-      {/* <Router> */}
-        <ul>
-          <li>
-            <Link to="/">About</Link>
-          </li>
-          <li>
-            <Link to="/Portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/Skills">Skills</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/Download">Download my CV</Link>
-          </li>
-        </ul>
-      {/* </Router> */}
+      <ul>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/Portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/Skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/Download">Download my CV</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
