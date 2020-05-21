cc5ae8d27f0b8b9d77486ce1d3414efe590064c1
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-21 15:27:44 +0100

    Add structure Skills component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,8 +1,53 @@
 import React from "react";
 import "./Skills.css";
+import Division from "../Division/Division.js";
 
 export default function Skills() {
   return (
-   <div>Placeholder Skills</div>
+    <div className="Skills">
+      <h2 className="heading">Skills</h2>
+      <Division />
+      <div className="container">
+        <div className="item-skills">
+          <h3>category</h3>
+          <ul>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+          </ul>
+        </div>
+        <div className="item-skills">
+          <h3>category</h3>
+
+          <ul>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+          </ul>
+        </div>
+        <div className="item-skills">
+          <h3>category</h3>
+
+          <ul>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+          </ul>
+        </div>
+        <div className="item-skills">
+          <h3>category</h3>
+
+          <ul>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+            <li>skill</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
