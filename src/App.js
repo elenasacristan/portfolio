f61585de322938d02cd9822715dfdafbde6ed602
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-21 12:55:58 +0100

    Add placeholder for Portfolio, Skills, contact and download CV

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 import "./Components/NavBar/NavBar.js";
 import NavBar from "./Components/NavBar/NavBar.js";
 import About from "./Components/About/About.js";
+import Portfolio from "./Components/Portfolio/Portfolio.js";
+import Skills from "./Components/Skills/Skills.js";
+import Contact from "./Components/Contact/Contact.js";
+import Download from "./Components/Download/Download.js";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <About />
+      <Portfolio />
+      <Skills />
+      <Contact />
+      <Download/>
     </div>
   );
 }
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Skills.css";
+
+export default function Skills() {
+  return (
+   <div>Placeholder Skills</div>
+  );
+}
