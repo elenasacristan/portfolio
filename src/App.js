8008877169615f857f7c83ecc3a3852c15cf1382
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-21 15:09:33 +0100

    Add routing / navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./Components/NavBar/NavBar.js";
 import NavBar from "./Components/NavBar/NavBar.js";
 import About from "./Components/About/About.js";
@@ -8,16 +9,19 @@ import Skills from "./Components/Skills/Skills.js";
 import Contact from "./Components/Contact/Contact.js";
 import Download from "./Components/Download/Download.js";
 
-
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <About />
-      <Portfolio />
-      <Skills />
-      <Contact />
-      <Download/>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={About}></Route>
+          <Route path="/Portfolio" component={Portfolio}></Route>
+          <Route path="/Skills" component={Skills}></Route>
+          <Route path="/Contact" component={Contact}></Route>
+          <Route path="/Download" component={Download}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
@@ -7,12 +8,25 @@ export default function NavBar() {
       <div className="logo">
         <span>REPLACE with LOGO</span>
       </div>
-      <ul>
-        <li>Portfolio</li>
-        <li>Skills</li>
-        <li>Contact</li>
-        <li>Download my CV</li>
-      </ul>
+      {/* <Router> */}
+        <ul>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/Portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/Skills">Skills</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/Download">Download my CV</Link>
+          </li>
+        </ul>
+      {/* </Router> */}
     </nav>
   );
 }
