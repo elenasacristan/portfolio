8dd48a43d033cb2f7c9e3e2ebf9c8c6f66355508
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-03 17:05:03 +0100

    Improve NavBar responsiveness

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,40 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
-import { FaCode } from "react-icons/fa";
-import { HashLink as Link } from "react-router-hash-link";
+import { FaCode, FaAlignJustify } from "react-icons/fa";
+import { NavHashLink as Link } from "react-router-hash-link";
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="NavBar" id="navbar">
-      <div className="logo">
-        <span>
-          <Link smooth to="#about">
-            <FaCode className="logo-icon" color="#ffc355" size="3.2rem" />
-          </Link>
-        </span>
-      </div>
-      <ul>
-        <li>
-          <Link smooth to="#about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link smooth to="#portfolio">
-            Portfolio
-          </Link>
-        </li>
-        <li>
-          <Link smooth to="#skills">
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link smooth to="#contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="NavBar">
+        <div className="logo">
+          <span>
+            <Link smooth to="#about" activeClassName="selected">
+              <FaCode className="logo-icon" color="#ffc355" size="3.2rem" />
+            </Link>
+          </span>
+        </div>
+        <div className="NavBar-continer-button-menu">
+          <button className="NavBar-button" onClick={() => setIsOpen(!isOpen)}>
+            <FaAlignJustify className="hamburger" />
+          </button>
+          <div className={`NavBar-box ${isOpen ? "is-open" : ""}`}>
+            <div className="NavBar-box-link" onClick={() =>setIsOpen(false)}>
+              <Link smooth to="#about">
+                About
+              </Link>
+            </div>
+            <div className="NavBar-box-link" onClick={() =>setIsOpen(false)}>
+              <Link smooth to="#portfolio">
+                Portfolio
+              </Link>
+            </div>
+            <div className="NavBar-box-link" onClick={() =>setIsOpen(false)}>
+              <Link smooth to="#skills">
+                Skills
+              </Link>
+            </div>
+            <div className="NavBar-box-link" onClick={() =>setIsOpen(false)}>
+              <Link smooth to="#contact">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+    </div>
   );
 }
+
+// export default function NavBar() {
+//   return (
+//     <nav className="NavBar" id="navbar">
+//       <div className="logo">
+//         <span>
+//           <Link smooth to="#about">
+//             <FaCode className="logo-icon" color="#ffc355" size="3.2rem" />
+//           </Link>
+//         </span>
+//       </div>
+//       <ul>
+//         <li>
+//           <Link smooth to="#about">
+//             About
+//           </Link>
+//         </li>
+//         <li>
+//           <Link smooth to="#portfolio">
+//             Portfolio
+//           </Link>
+//         </li>
+//         <li>
+//           <Link smooth to="#skills">
+//             Skills
+//           </Link>
+//         </li>
+//         <li>
+//           <Link smooth to="#contact">
+//             Contact
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
