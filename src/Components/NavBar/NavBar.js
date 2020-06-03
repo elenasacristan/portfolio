3b33f101438f1b980f1e703a850182fadb39c5c4
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-03 11:50:18 +0100

    Add react-router-hash-link to bookmark sections and add keeping smooth scrolling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,65 +1,36 @@
 import React from "react";
 import "./NavBar.css";
 import { FaCode } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-import { Link } from "react-scroll";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default function NavBar() {
   return (
     <nav className="NavBar" id="navbar">
       <div className="logo">
         <span>
-          <Link to="about" smooth={true} offset={-60} duration={600}>
+          <Link smooth to="#about">
             <FaCode className="logo-icon" color="#ffc355" size="3.2rem" />
           </Link>
         </span>
       </div>
       <ul>
         <li>
-          <Link
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={-60}
-            duration={600}
-          >
+          <Link smooth to="#about">
             About
           </Link>
         </li>
         <li>
-          <Link
-            activeClass="active"
-            to="portfolio"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={600}
-          >
+          <Link smooth to="#portfolio">
             Portfolio
           </Link>
         </li>
         <li>
-          <Link
-            activeClass="active"
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={600}
-          >
+          <Link smooth to="#skills">
             Skills
           </Link>
         </li>
         <li>
-          <Link
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
+          <Link smooth to="#contact">
             Contact
           </Link>
         </li>
