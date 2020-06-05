921fe24d643f41bc4475b4876caa69d08ba391bd
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-05 19:57:58 +0100

    Add details to Details component

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,32 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Division from "../Division/Division.js";
-import Project from "../Project/Project.js";
 import ProjectData from "../Portfolio/Projects-data.js";
+import "./Details.css";
+import { FaRegEye, FaGithub } from "react-icons/fa";
 
-export default function Portfolio({ id, match }) {
+export default function Details({ match }) {
   const [project, setProject] = useState({});
   useEffect(() => {
     let projectSelected = ProjectData().filter(
       (project) => project.id === parseInt(match.params.idProject)
     );
     setProject(projectSelected[0]);
-    console.log(projectSelected[0].technologies)
+    console.log(projectSelected[0].technologies);
   }, []);
   return (
-    <div className="Portfolio" id={id}>
+    <div className="Details">
       <h2 className="heading">{project.title}</h2>
       <Division />
-      <div className="container">
-        <img src={project.img} width="200" />
-        <br />
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-        <br />
-        <p>Link website</p> <p>Link github</p>
+      <div className="Details-container">
+        <div className="Details-image">
+          <img src={project.img} width="200" />
+        </div>
+        <div className="Details-body">
+          <div className="Description">
+            {project.description}
+          </div>
+          <div className="Details-icons-container">
+            <FaRegEye className="icon" />
+            <FaGithub className="icon" />
+          </div>
+        </div>
         <div>
           {/* {project.technologies.map((technology) => (
             <img
@@ -41,8 +44,3 @@ export default function Portfolio({ id, match }) {
     </div>
   );
 }
-
-// technologies: [Html, Css, JavaScript, React, Nodejs, Express],
-// url: "https://kodflix-ess.herokuapp.com/",
-// github: "https://github.com/elenasacristan/kodflix",
-// img: Kodflix,
