c3f4619a5e30b0e33ccc79fdbccc73218dcd3837
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-05 20:30:48 +0100

    Add back to project button and update urls with title instead of id

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Details from "./Components/Details/Details.js";
 import Main from "./Components/Main/Main.js";
 
-// import NavBar from "./Components/NavBar/NavBar.js";
-// import About from "./Components/About/About.js";
-// import Portfolio from "./Components/Portfolio/Portfolio.js";
-// import Skills from "./Components/Skills/Skills.js";
-// import Contact from "./Components/Contact/Contact.js";
-
 function App() {
   return (
     <div className="App">
--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from "react";
 import Division from "../Division/Division.js";
 import ProjectData from "../Portfolio/Projects-data.js";
 import "./Details.css";
-import { FaRegEye, FaGithub } from "react-icons/fa";
+import { FaRegEye, FaGithub, FaArrowAltCircleLeft } from "react-icons/fa";
+// import { Link } from "react-router-dom";
+import { NavHashLink as Link } from "react-router-hash-link";
+
 
 export default function Details({ match }) {
   const [project, setProject] = useState({});
   useEffect(() => {
     let projectSelected = ProjectData().filter(
-      (project) => project.id === parseInt(match.params.idProject)
+      (project) => project.title === match.params.idProject
     );
     setProject(projectSelected[0]);
     console.log(projectSelected[0].technologies);
   }, []);
   return (
     <div className="Details">
+      <Link className="back-button" to="/#portfolio">
+        <FaArrowAltCircleLeft className="icon" /> Go back to the projects page
+      </Link>
       <h2 className="heading">{project.title}</h2>
       <Division />
       <div className="Details-container">
@@ -22,23 +28,11 @@ export default function Details({ match }) {
           <img src={project.img} width="200" />
         </div>
         <div className="Details-body">
-          <div className="Description">
-            {project.description}
-          </div>
+          <div className="Description">{project.description}</div>
           <div className="Details-icons-container">
             <FaRegEye className="icon" />
             <FaGithub className="icon" />
           </div>
-        </div>
-        <div>
-          {/* {project.technologies.map((technology) => (
-            <img
-              key={technology}
-              className="technologies"
-              src={technology}
-              alt=""
-            />
-          ))} */}
         </div>
       </div>
     </div>
