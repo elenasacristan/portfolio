832ffc6ec9d3b14fe5be6511a1557da6fc8a17e3
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-07-01 12:21:40 +0100

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import ReactGA from "react-ga";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Details from "./Components/Details/Details.js";
 import Main from "./Components/Main/Main.js";
 
+function initializeAnalytics() {
+  ReactGA.initialize("UA-171425151-1", {gaOptions: {
+    siteSpeedSampleRate: 100
+  }});
+  ReactGA.pageview('/')
+}
+
 function App() {
+  initializeAnalytics();
   return (
     <div className="App">
       <Router>
