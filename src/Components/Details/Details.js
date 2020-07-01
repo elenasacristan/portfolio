bea49af9d7dd4b2d301c2afd9e4a278b922a609b
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-07-01 12:32:40 +0100

    import ReactGA in Details.js

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactGA from "react-ga";
 import Division from "../Division/Division.js";
 import ProjectData from "../Portfolio/Projects-data.js";
 import "./Details.css";
